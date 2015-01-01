879ca001ad5370fe7ce5a28c63b996411aa6b329
Brad Hess <bhess@osisoft.com>
2015-01-01 00:35:30 -0500

    Push mods

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿// <copyright file="AssemblyInfo.cs" company="Brad Hess">
+//   Copyright (c) 2014 Brad Hess, and licensed under the MIT License.
+//   See 'LICENSE' in the repo root for more information.
+// </copyright>
+
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("BrowningStyle")]
 [assembly: AssemblyDescription("A collection of StyleCop rules for the detail-oriented developer.")]
 [assembly: AssemblyProduct("BrowningStyle")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Brad Hess")]
+[assembly: AssemblyCompany("Brad Hess")]
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: Guid("5a39cdb1-2511-47c1-8f2b-fdea68a0a1fd")]
--- src/Rules.cs
+++ src/Rules.cs
@@ -1,15 +1,109 @@
-﻿using System;
+﻿// <copyright file="Rules.cs" company="Brad Hess">
+//   Copyright (c) 2014 Brad Hess, and licensed under the MIT License.
+//   See 'LICENSE' in the repo root for more information.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using StyleCop;
+using StyleCop.CSharp;
 
 namespace BrowningStyle
 {
-    internal enum Rules
+    /// <summary>
+    /// A <see cref="SourceAnalyzer" /> that applies our custom rules.
+    /// </summary>
+    [SourceAnalyzer(typeof(CsParser))]
+    public class Rules : SourceAnalyzer
     {
-        NoTrailingWhitespace,
-        FileMustEndWithNewline,
-        MaximumLineLengthExceeded,
-        DoNotUseVarKeyword
+        /// <inheritdoc/>
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            Param.RequireNotNull(document, "document");
+            CsDocument csharpDocument = (CsDocument)document;
+
+            if (csharpDocument.RootElement != null && !csharpDocument.RootElement.Generated)
+            {
+                this.CheckLayout(csharpDocument);
+                csharpDocument.WalkDocument(this.ProcessElement, this.ProcessStatement);
+            }
+        }
+
+        /// <summary>
+        /// Checks the correctness of code text within a given document.
+        /// </summary>
+        /// <param name="document">The document to process.</param>
+        private void CheckLayout(CsDocument document)
+        {
+            using (TextReader reader = document.SourceCode.Read())
+            {
+                string[] lines = reader.ReadToEnd().Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+                if (!string.IsNullOrEmpty(lines[lines.Length - 1]))
+                {
+                    this.AddViolation(document.RootElement, lines.Length - 1, "FileMustEndWithNewline");
+                }
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if ("\\s$".IsMatch(lines[i]))
+                    {
+                        this.AddViolation(document.RootElement, i + 1, "NoTrailingWhitespace");
+                    }
+
+                    if (lines[i].Length > 120)
+                    {
+                        this.AddViolation(document.RootElement, i + 1, "MaximumLineLengthExceeded");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Processes one element. This is a no-op implementation.
+        /// </summary>
+        /// <param name="element">The element to process.</param>
+        /// <param name="parentElement">The parent element.</param>
+        /// <param name="context">The processing context.</param>
+        /// <returns>Returns false if the analyzer should quit.</returns>
+        private bool ProcessElement(CsElement element, CsElement parentElement, object context)
+        {
+            Param.Ignore(element, parentElement, context);
+            return true;
+        }
+
+        /// <summary>
+        /// Processes one statement.
+        /// </summary>
+        /// <param name="statement">The statement to process.</param>
+        /// <param name="parentExpression">The parent expression.</param>
+        /// <param name="parentStatement">The parent statement.</param>
+        /// <param name="parentElement">The parent element.</param>
+        /// <param name="context">The processing context.</param>
+        /// <returns>Returns false if the analyzer should quit.</returns>
+        private bool ProcessStatement(
+            Statement statement,
+            Expression parentExpression,
+            Statement parentStatement,
+            CsElement parentElement,
+            object context)
+        {
+            Param.AssertNotNull(statement, "statement");
+            Param.AssertNotNull(parentElement, "parentElement");
+            Param.Ignore(parentExpression, parentStatement, context);
+
+            if (statement.StatementType == StatementType.VariableDeclaration)
+            {
+                foreach (CsToken token in statement.Tokens.Where(
+                    x => x.CsTokenType == CsTokenType.Other && x.Text == "var"))
+                {
+                    this.AddViolation(parentElement, token.Location, "DoNotUseVarKeyword");
+                }
+            }
+
+            return true;
+        }
     }
 }
