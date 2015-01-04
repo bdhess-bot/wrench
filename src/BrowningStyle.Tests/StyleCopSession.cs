5524aaa91e3645d671c6f16bca2479e14f907189
Brad Hess <bradley.d.hess@gmail.com>
2015-01-04 14:52:03 -0500

    more diags

--- src/BrowningStyle.Tests/StyleCopSession.cs
+++ src/BrowningStyle.Tests/StyleCopSession.cs
@@ -107,6 +107,9 @@ namespace BrowningStyle.Tests
                 contents = reader.ReadToEnd();
             }
 
+            Console.WriteLine("The platform new line character is: {0}",
+                Environment.NewLine == "\r\n" ? @"\r\n" : @"\n");
+
             Console.WriteLine(filename + ":");
             Console.WriteLine(contents);
             Console.WriteLine("###");
